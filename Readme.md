1df4ce424ee8d1a8cbae1e557a759b97308d35d0
Lorenatacal <lorena.tacal@gmail.com>
2018-04-29 15:04:56 +0100

    Add Readme.md

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#Advanced Katas
+This is a FreeCodeCamp katas that made me understand JavaScript better
---
+++ ValidateTelephoneNumbers.js
@@ -0,0 +1,13 @@
+function telephoneCheck(str) {
+    // Good luck!
+    var reg = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/;
+    if(reg.test(str)){
+      return true;
+       }else{
+       return false;
+       }
+  }
+  
+  
+  
+  console.log(telephoneCheck("555-555-5555"));
